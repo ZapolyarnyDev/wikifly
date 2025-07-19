56290778aa0049b92f1365894b5c2ead2bd93953
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-07-19 21:15:35 +0300

    feat(types): Add basic interfaces for Wikipedia articles and sections

---
+++ src/models/wiki-article.ts
@@ -0,0 +1,11 @@
+import { WikiArticleSection } from "./wiki-section";
+
+export interface WikiArticle {
+    readonly title: string,
+    readonly intro?: string,
+    readonly imageUrl?: string,
+    readonly articleUrl: string,
+    readonly languageCode: string,
+    readonly lastModified: string,
+    readonly articleContent?: WikiArticleSection[]
+}
